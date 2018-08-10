4d3de4be572d3bed9d1f7fb9c83c27a36ad4dde4
Antonius Sopian <antonius.sopian@gmail.com>
2018-08-10 21:01:26 +1000

    5. Pagination

--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -11,9 +11,16 @@ import { PokemonService } from '../pokemon.service';
 export class PokemonsComponent implements OnInit {
 
   pokemonsFull: Pokemon[];
+  pokemonsDisplayed: Pokemon[];
+
+  currentPage: number;
+  totalPages: number;
+  ITEMS_PER_PAGE = 20;
+
   constructor(private pokemonService: PokemonService) { }
 
   ngOnInit() {
+    this.currentPage = 1;
     this.getPokemons();
   }
 
@@ -21,6 +28,30 @@ export class PokemonsComponent implements OnInit {
     this.pokemonService.getPokemons()
       .subscribe(pokemons => {
         this.pokemonsFull = pokemons;
+        this.updateDisplayedPokemons();
       });
   }
+
+  updateDisplayedPokemons(): void {
+    this.totalPages = Math.ceil(this.pokemonsFull.length / this.ITEMS_PER_PAGE);
+
+    if (this.currentPage < 1) this.currentPage = 1;
+    if (this.currentPage > this.totalPages) this.currentPage = this.totalPages;
+
+    this.pokemonsDisplayed =
+      this.pokemonsFull
+        .slice((this.currentPage-1) * this.ITEMS_PER_PAGE)
+        .slice(0, this.ITEMS_PER_PAGE);
+  }
+
+  nextPage(): void {
+    this.currentPage = this.currentPage + 1;
+    this.updateDisplayedPokemons();
+  }
+
+  previousPage(): void {
+    this.currentPage = this.currentPage - 1;
+    this.updateDisplayedPokemons();
+  }
+
 }
