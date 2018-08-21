7ee7bb6db795fd217cd2908f918e489299a35054
Antonius Sopian <antonius.sopian@gmail.com>
2018-08-21 19:41:47 +1000

    Refactor-out the header into separate component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { PokemonsComponent } from './pokemons/pokemons.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PokemonsComponent } from './pokemons/pokemons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     PokemonsComponent
   ],
   imports: [
