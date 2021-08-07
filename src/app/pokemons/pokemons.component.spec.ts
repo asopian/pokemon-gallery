7a3d0c5e98ac26c3fa5529b982bfa5acda94c9ac
Antonius Sopian <antonius.sopian@gmail.com>
2021-08-07 18:36:47 +1000

    Upgrade to Angular 11.2.14

--- src/app/pokemons/pokemons.component.spec.ts
+++ src/app/pokemons/pokemons.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PokemonsComponent } from './pokemons.component';
 
@@ -6,7 +6,7 @@ describe('PokemonsComponent', () => {
   let component: PokemonsComponent;
   let fixture: ComponentFixture<PokemonsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PokemonsComponent ]
     })
